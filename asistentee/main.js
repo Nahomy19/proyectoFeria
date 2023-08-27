3aef0b1d74396d148076d0a5fe3466923f94e8d1
Nahomy19 <140674922+Nahomy19@users.noreply.github.com>
2023-08-27 15:46:50 -0600

    gsgs

--- asistentee/main.js
+++ asistentee/main.js
@@ -14,7 +14,7 @@ annyang.addCallback('soundstart', function () {
     if (!bandera){
         document.getElementById("all2").style.display="block"
         setTimeout(() => {
-            voz('Bienvenido de nuevo')
+            voz('Bienvenido de nuevo Usuario')
             bandera = true;
         }, 1000);
     }
@@ -32,25 +32,41 @@ const comandos = {
         voz("Bienvenido de nuevo");
     },
 
+    "Mochi": () => {
+        voz("Hola, en qué puedo ayudar");
+    },
+
     "hey Mochi": () => {
-        voz("Bienvenido de nuevo");
+        voz("Digame, en que le puedo ayudar");
     },
 
-    "Buenos días Mochi": () => {
-        voz("Buenps días, bienvenido de nuevo");
+    "Saluda a las personas": () => {
+        voz("Bienvenidos soy Mochi, un asistente virtual");
     },
 
-    "Buenas tardes Mochi": () => {
+    "Mochi Buenos días": () => {
+        voz("Buenos días, Espero que estés bien");
+    },
+
+    "Mochi Buenas tardes": () => {
         voz("Buenas tardes, bienvenido de nuevo");
     },
 
-    "Buenas noches Mochi": () => {
+    "Buenas tardes Mochi": () => {
+        voz("Buenas tardes");
+    },
+
+    "Mochi Buenas noches": () => {
         voz("Buenas noches, bienvenido de nuevo");
+    },
+
+    "Buenas noches Mochi": () => {
+        voz("Buenas noches");
     },
 
     // DESPEDIDA
 
-    "Hasta mañana Mochi": () => {
+    "Mochi Hasta mañana": () => {
         voz("Hasta mañana");
         annyang.abort()
     },
@@ -60,21 +76,21 @@ const comandos = {
         annyang.abort()
     },
 
-    "Adios Mochi": () => {
+    "Adios mochi": () => {
         voz("Hasta luego");
         annyang.abort()
     },
 
-    "Descansa Mochi": () => {
+    "mochi Descansa": () => {
         voz("Descansa tú también")
     },
 
-    "apágate": () => {
+    "mochi apágate": () => {
         voz('ok, hasta luego')
         annyang.abort();
     },
 
-    "apágate por *tiempo minutos": tiempo => {
+    "mochi apágate por *tiempo minutos": tiempo => {
         voz('ok, vuelvo en' + tiempo + 'minutos');
         annyang.abort();
         setTimeout(() => {
@@ -85,7 +101,7 @@ const comandos = {
 
     // PREGUNTAS
 
-    "qué hora es": () => {
+    "Mochi qué hora es": () => {
         var date = new Date;
         var hours = date.getHours();
         var minutes = date.getMinutes();
@@ -98,11 +114,11 @@ const comandos = {
         voz('son las ' + strTime)
     },
 
-    "quién te creo": () => {
+    "Mochi quién te creo": () => {
         voz("El equipo Dragon Works");
     },
 
-    "qué eres": () => {
+    "mochi qué eres": () => {
         voz("soy un asistente virtual");
     },
 
@@ -110,61 +126,31 @@ const comandos = {
         voz("mi nombre es Mochi");
     },
 
-    "qué fecha es hoy": () => {
+    "como te llamas": () => {
+        voz("mi nombre es Mochi");
+    },
+
+    "mochi qué fecha es hoy": () => {
         var date = new Date;
         var mes = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
         voz("hoy es " + date.getDate() + " de "+ mes[date.getMonth()] + "del" + date.getFullYear());
     },
 
-    "qué día es hoy": () => {
+    "mochi qué día es hoy": () => {
         var date = new Date;
         var dia = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
         voz("hoy es "+ dia[date.getDay()-1]);
     },
 
-    "Qué es una arquitectura de software": () => {
-        voz("Una arquitectura de software es la estructura o diseño fundamental de un sistema de software que define la organización de sus componentes, sus relaciones y las reglas que guían su evolución");
-    },
-
-    "Qué es la refactorización en el desarrollo de software": () => {
-        voz("La refactorización es una práctica que consiste en mejorar el código existente sin cambiar su comportamiento externo. Se realiza para aumentar la legibilidad, reducir la complejidad y mejorar el rendimiento del software sin introducir errores");
-    },
-
-    "Qué es un framework": () => {
-        voz("Un framework  es una herramienta que da acceso a los desarrolladores de software a componentes o soluciones prefabricadas diseñadas para agilizar el desarrollo");
-    },
-
-    "Qué es programar": () => {
-        voz("La programación se trata del proceso que se aplica cuando se crea algún tipo de aplicación o software, o para llevar a cabo algún concepto o proyecto que necesita del uso de un lenguaje informático");
-    },
-
-    "Cúales son las etapas del ciclo de vida del desarrollo de software": () => {
-        voz("Planificación, Diseño, Recopilación de análisis de requisitos, Pruebas, Despliegue, Mantenimiento");
-    },
-
-    "Qué es la programación orientada a objetos": () => {
-        voz("Es un modelo de programación en el que el diseño de software se organiza alrededor de datos o objeto, en vez de dar funciones ilógicas, se enfoca a los objetos de los programadores necesitan manipular");
-    },
-
-    "Qué es un modelo entidad de relación": () => {
-        voz("Un diagrama entidad-relación también conocido como modelo antidad relación o ORD, es un tipo de diagrama de flujo que ilustra como las entidades, como personas, objetos, o concepto, se relacionan entre si dentro de un sistema");
-    },
-
-    "Qué es un lenguaje de programación": () => {
-        voz("un lenguaje de programación es una herramienta que permite desarrollar software o programas para computadora. Los lenguajes de programación son empleados para diseñar e implementar programas encargados de definir y administrar el comportamiento de los dispositivos físicos y lógicos de una computadora");
-    },
-
-    "Qué es un pseudocódigo": () => {
-        voz("Es una manera de definir los algoritmos, los pasos necesarios para resolver un problema. El pseudocódigo es un paso intermedio entre los diagramas de flujo, que se expresan mediante símbolos, y los lenguajes de programación, que están ligados a una sintaxis bien definida");
-    },
-
-    "Qué es la depuración": () => {
-        voz("La depuración es el proceso de descubrir y eliminar errores en la fuente de software de un programa de computadora");
+    "mochi qué día es": () => {
+        var date = new Date;
+        var dia = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
+        voz("hoy es "+ dia[date.getDay()-1]);
     },
 
     // ORDENES
 
-    "cuéntame un chiste": () => {
+    "Mochi cuéntame un chiste": () => {
         var chistes = ["¿Por qué las focas del circo miran siempre hacia arriba?, Porque es donde están los focos",
             "¡Estás obsesionado con la comida!, No sé a que te refieres croquetamente",
             "¿Por qué estás hablando con esas zapatillas?, Porque pone converse",
@@ -179,17 +165,36 @@ const comandos = {
         voz(chistes[ran])
     },
 
-    "reiniciate": () => {
+    "Mochi reiniciate": () => {
         voz("entendido");
         location.reload();
     },
 
-    "limpia la consola": () => {
+    "Mochi limpia la consola": () => {
         voz("entendido");
         console.clear();
     },
 
-    "di *frase": frase => {
+    /*
+    "Mochi busca *busqueda": busqueda => {
+        voz("ok, buscando " + busqueda +" para ti");
+        window.open("https://www.google.com/search?q=" + busqueda)
+    },*/
+
+    "Mochi busca *busqueda": busqueda => {
+        const mensaje = "OK, buscando " + busqueda + " para ti";
+        responsiveVoice.speak(mensaje, "Spanish Female"); // Cambia "Spanish Female" al idioma/voz que prefieras
+        const respuestaCompleta = "Aquí tienes algunos resultados";
+        responsiveVoice.speak(respuestaCompleta, "Spanish Female");
+        window.open("https://www.google.com/search?q=2djoZL2lJearwbkPhe6TyAo&oq= _lp=Egxnd3Mtd2l6LXNlcnAiEFF1w6kgZXMgc29mdHdhcmVIAFAAWABwAHgBkAEAmAEAoAEAqgEAuAESyAEA-AEG4gMEGAAgQQ&gs_ivs=1&sclient=gws-wiz-serp#tts=0" + busqueda);
+    },
+    
+    "Mochi reproduce *busqueda": busqueda => {
+        voz("ok, reproduciendo " + busqueda + "para ti");
+        window.open("https://www.youtube.com/watch?v=" + busqueda)
+    },
+
+    "mochi di *frase": frase => {
         voz(frase);
     },
     "escribe *dicto": dicto =>{
@@ -198,38 +203,62 @@ const comandos = {
 
     // CORTESÍA
 
+    "gracias mochi": () => {
+        voz("Para servirte");
+    },
+
     "gracias": () => {
         voz("Para servirte");
     },
 
-    "Cómo estás": () => {
+    "muchas gracias": () => {
+        voz("Para servirte");
+    },
+
+    "Mochi gracias": () => {
+        voz("Para servirte");
+    },
+
+    "mochi muchas gracias": () => {
+        voz("Para servirte");
+    },
+
+    "Mochi Cómo estás": () => {
         voz('mejor que ayer, espero que tú también lo estés')
     },
 
-    "Te presento a *nombre": nombre => {
+    "Mochi Te presento a *nombre": nombre => {
         voz("Hola" + nombre +", mi nombre es Mochi, es un placer conocerte");
     },
 
     // LLAMADA A LA ACCIÓN
     
-    "Mochi": () => {
-        voz("aquí estoy");
+    "Hola Mochi": () => {
+        voz("Hola mucho gusto");
     },
 
     "Hey": () => {
         voz("aquí estoy");
     },
 
-    "Hola": () => {
+    "Hey Mochi": () => {
         voz("aquí estoy");
+    },    
+
+    "Hola": () => {
+        voz("Hola mucho gusto");
     },
 
-    "Me puedes ayudar": () => {
+    "mochi Hola": () => {
+        voz("Hola mucho gusto");
+    },
+
+    "Mochi Me puedes ayudar": () => {
         voz("claro que sí");
     },
 
-    "Oye": () => {
-        voz("aquí estoy");
+    "Oye mochi": () => {
+        voz("Si, lo escucho");
     },
 
     "Mochi estás ahí": () => {
